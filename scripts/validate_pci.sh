3493fe53de63155484cc2fd9bd88a9e41803d5d2
skr003 <78012061+skr003@users.noreply.github.com>
2025-08-27 01:27:40 +0530

    Update validate_pci.sh

--- scripts/validate_pci.sh
+++ scripts/validate_pci.sh
@@ -2,7 +2,7 @@
 set -e
 
 INPUT="output/azure.json"
-POLICY="policies/pci_dss.rego"
+POLICY="policy/azure/pci_dss.rego"
 
 echo "[*] Running PCI DSS validation with OPA..."
 opa eval --format pretty --input $INPUT --data $POLICY "data.pci_dss.deny"
