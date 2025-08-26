22466c22dc91504719ee59808ae01cbcd96802b9
skr003 <78012061+skr003@users.noreply.github.com>
2025-08-26 23:24:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ terraform {
   }
   required_version = ">= 1.3.0"
 }
+
 provider "azurerm" {
   features {}
 }
@@ -145,64 +146,6 @@ resource "azurerm_network_watcher_flow_log" "nsg_flow_log" {
 
 # -----------------------
 # Storage Account (hardened)
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_key_vault" "example" {
-  name                = "examplekv"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
-  purge_protection_enabled = true
-}
-
-resource "azurerm_key_vault_access_policy" "client" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
-  secret_permissions = ["Get"]
-}
-
-resource "azurerm_key_vault_key" "example" {
-  name         = "tfex-key"
-  key_vault_id = azurerm_key_vault.example.id
-  key_type     = "RSA"
-  key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-
-  depends_on = [
-    azurerm_key_vault_access_policy.client
-  ]
-}
-
-
-resource "azurerm_storage_account" "ok_storage_account" {
-  name                     = "examplestor"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-
-resource "azurerm_storage_account_customer_managed_key" "ok_cmk" {
-  storage_account_id = azurerm_storage_account.ok_storage_account.id
-  key_vault_id       = azurerm_key_vault.example.id
-  key_name           = azurerm_key_vault_key.example.name
-}
-
 # -----------------------
 resource "azurerm_storage_account" "storage" {
   name                     = "pcidssstoragedemo" # must be globally unique; change if necessary
