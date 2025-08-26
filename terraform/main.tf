74388d931591e258e9892b83ffddae0da4ceaf4e
skr003 <78012061+skr003@users.noreply.github.com>
2025-08-26 23:54:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.5.0"
+      version = ">=3.0.0"
     }
   }
   required_version = ">= 1.3.0"
