84ee4b1016aa448d9f2720c927dec41ffc200a21
skr003 <78012061+skr003@users.noreply.github.com>
2025-08-26 23:04:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -295,12 +295,16 @@ resource "azurerm_network_interface" "nic" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
-  }
+ip_configuration {
+  name                          = "internal"
+  subnet_id                     = azurerm_subnet.example.id
+  private_ip_address_allocation = "Dynamic"
+}
+ip_configuration {
+  name                          = "internal2"
+  subnet_id                     = azurerm_subnet.example.id2
+  private_ip_address_allocation = "Dynamic"
+}
 }
 
 # -----------------------
