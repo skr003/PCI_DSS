71d140876b86501363b6a83f06d4978bd937feb9
skr003 <78012061+skr003@users.noreply.github.com>
2025-08-26 21:45:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -169,7 +169,7 @@ resource "azurerm_storage_account" "logging_sa" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
   min_tls_version          = "TLS1_2"
   allow_blob_public_access = false
   public_network_access_enabled = false
